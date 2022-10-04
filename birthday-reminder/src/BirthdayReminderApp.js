7f3ff2cdacf161715cc549cdb7dde143e3f0ca19
pauline-wanjiku <paulinendungushiko96@gmail.com>
2022-10-04 21:57:07 +0300

    Intial commit

---
+++ birthday-reminder/src/BirthdayReminderApp.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./style.css"
+
+function BirthdayReminderApp() {
+  return (
+    <>
+      <h1>Birthday Reminder</h1>
+      <section>
+        <h2>List</h2>
+        <button>Clear Button</button>
+      </section>
+    </>
+  );
+}
+export default BirthdayReminderApp;
---
+++ birthday-reminder/src/list.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const List=() =>{
+  return (
+      <h2>List Component</h2>
+  )
+}
+export default List
